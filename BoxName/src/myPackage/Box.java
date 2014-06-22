2ad2edbb84952b996869681cc12fe4cc657a5c0d
Antoinette-Thompson <tonithompson.tb@gmail.com>
2014-06-22 12:08:16 -0400

    Boxed Name Big Java Exercise

---
+++ BoxName/src/myPackage/Box.java
@@ -0,0 +1,13 @@
+package myPackage;
+
+public class Box 
+{
+	public static void main(String[] args)
+	{
+		//Print a name enclosed in a box.
+		
+		System.out.println("+----------+");
+		System.out.println("|Antoinette|");
+		System.out.println("+----------+");
+		}
+}
